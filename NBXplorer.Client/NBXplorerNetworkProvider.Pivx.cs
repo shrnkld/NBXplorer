c8c58c10fb2e253d5cf9d588815ad1f5d38a4302
Shorn <shrnkld@pm.me>
2023-07-18 14:22:49 +0200

    add support for pivx WIP

---
+++ NBXplorer.Client/NBXplorerNetworkProvider.Pivx.cs
@@ -0,0 +1,24 @@
+﻿using NBitcoin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NBXplorer
+{
+    public partial class NBXplorerNetworkProvider
+    {
+		private void InitPIVX(ChainName networkType)
+		{
+			Add(new NBXplorerNetwork(NBitcoin.Altcoins.PIVX.Instance, networkType)
+			{
+				MinRPCVersion = 140200,
+				CoinType = networkType == ChainName.Mainnet ? new KeyPath("2'") : new KeyPath("1'"),
+			});
+		}
+
+		public NBXplorerNetwork GetPIVX()
+		{
+			return GetFromCryptoCode(NBitcoin.Altcoins.PIVX.Instance.CryptoCode);
+		}
+	}
+}
